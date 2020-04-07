6325e786a88d31369ae4d536cd564da4b88c254b
Kareem Elkasaby <elkasaby@localhost.localdomain>
2020-04-07 22:31:31 +0200

    changed

--- app/models/forder.rb
+++ app/models/forder.rb
@@ -2,5 +2,5 @@ class Forder < ApplicationRecord
     validates :meal, :presence => true, :length => { :maximum => 50, :minimum => 3 }
     validates :resturant, :presence => true, :length => { :maximum => 50 , :minimum => 3}
     validates :image, :presence => true, :length => { :maximum => 50 }
-    belongs_to :model, class_name: "model", foreign_key: "model_id"
+    belongs_to :model, class_name: "Model", foreign_key: "model_id"
 end
