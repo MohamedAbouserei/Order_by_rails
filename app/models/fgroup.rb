e40c76f4867dab00e8c698dbb24d2d7504dd6afc
Mohamed Atef <nerophone2012@gmail.com>
2020-04-07 23:19:25 +0200

    validate fgroup

--- app/models/fgroup.rb
+++ app/models/fgroup.rb
@@ -1,2 +1,5 @@
 class Fgroup < ApplicationRecord
+    validates :name , :presence => true , length: {maximum:50,minimum:3} , :uniqueness =>  true 
+    validates :model_id , :presence => true , :uniqueness =>  true
+    belongs_to :model
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -3,4 +3,6 @@ class Model < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :fgroups, class_name: "fgroup", foreign_key: "model_id"
+  
 end
