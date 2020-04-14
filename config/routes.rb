6a67adf9204cfce9ed19e18ba47c14477de3a246
Mohamed Atef <nerophone2012@gmail.com>
2020-04-15 01:00:51 +0200

    invite friends v1

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   devise_for :models
   get '/auth/:provider/callback' => 'sessions#omniauth'
   get 'auth/failure', to: redirect('/')
+  get '/addusertogroup',to: 'fgroups#add'
+
 
 
   
