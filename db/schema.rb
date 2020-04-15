bdfe81a8118eb0c5f63a10523cd4c408f0089501
Mohamed Atef <nerophone2012@gmail.com>
2020-04-15 03:10:27 +0200

    invite friends v2

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_04_14_164329) do
 
-  create_table "fgroups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "fgroups", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_164329) do
     t.index ["model_id"], name: "index_fgroups_on_model_id"
   end
 
-  create_table "forders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "forders", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "meal", limit: 50, null: false
     t.string "resturant", limit: 50, null: false
     t.string "image", limit: 50
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_164329) do
     t.index ["model_id"], name: "index_forders_on_model_id"
   end
 
-  create_table "friends", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "friends", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.boolean "action", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -40,16 +40,16 @@ ActiveRecord::Schema.define(version: 2020_04_14_164329) do
     t.index ["request_id"], name: "index_friends_on_request_id"
   end
 
-  create_table "groupuser", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "groupusers", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "model_id", null: false
     t.bigint "fgroup_id", null: false
-    t.index ["fgroup_id"], name: "index_groupuser_on_fgroup_id"
-    t.index ["model_id"], name: "index_groupuser_on_model_id"
+    t.index ["fgroup_id"], name: "index_groupusers_on_fgroup_id"
+    t.index ["model_id"], name: "index_groupusers_on_model_id"
   end
 
-  create_table "invitations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "invitations", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "owner_id"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_164329) do
     t.index ["owner_id"], name: "index_invitations_on_owner_id"
   end
 
-  create_table "models", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "models", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.boolean "admin", default: false
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 2020_04_14_164329) do
     t.index ["username"], name: "index_models_on_username", unique: true
   end
 
-  create_table "notifcations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "notifcations", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "link"
     t.string "title", limit: 50, default: "", null: false
     t.string "color", default: "blue"
@@ -90,26 +90,26 @@ ActiveRecord::Schema.define(version: 2020_04_14_164329) do
     t.index ["model_id"], name: "index_notifcations_on_model_id"
   end
 
-  create_table "orderuser", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "orderusers", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "model_id", null: false
     t.bigint "forder_id", null: false
     t.string "meal"
-    t.index ["forder_id"], name: "index_orderuser_on_forder_id"
-    t.index ["model_id"], name: "index_orderuser_on_model_id"
+    t.index ["forder_id"], name: "index_orderusers_on_forder_id"
+    t.index ["model_id"], name: "index_orderusers_on_model_id"
   end
 
   add_foreign_key "fgroups", "models"
   add_foreign_key "forders", "models"
   add_foreign_key "friends", "models", column: "reciver_id"
   add_foreign_key "friends", "models", column: "request_id"
-  add_foreign_key "groupuser", "fgroups"
-  add_foreign_key "groupuser", "models"
+  add_foreign_key "groupusers", "fgroups"
+  add_foreign_key "groupusers", "models"
   add_foreign_key "invitations", "fgroups"
   add_foreign_key "invitations", "models"
   add_foreign_key "invitations", "models", column: "owner_id"
   add_foreign_key "notifcations", "models"
-  add_foreign_key "orderuser", "forders"
-  add_foreign_key "orderuser", "models"
+  add_foreign_key "orderusers", "forders"
+  add_foreign_key "orderusers", "models"
 end
